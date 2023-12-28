b0515ba69d465525f3bfda51d8c41e8828725ef3
zhoutaosheng <zhoutaosheng@cloudcsp.com>
2023-12-28 10:53:22 +0800

    pass

--- examples/pom_write.rs
+++ examples/pom_write.rs
@@ -1,8 +1,10 @@
-use project_gen::java::{POMWriter, POM};
+use project_gen::java::{POMWriter, POM, Property};
 
 fn main() {
     let mut pom_writer = POMWriter::default();
     let mut pom = POM::new("cn.ljyun", "test");
+    pom.add_property(Property::new("maven.compiler.source","8"));
+    pom.add_property(Property::new("maven.compiler.target","8"));
     let root_dir = std::env::current_dir().unwrap();
     let dir = root_dir.join("java");
     pom_writer.write_to(&pom,dir);
